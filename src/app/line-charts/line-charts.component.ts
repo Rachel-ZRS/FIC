36341710195c95e2d4a1e0178a67e09b5e465f42
Rachel Zhang <rachelzhang@Rachels-MacBook-Air.local>
2020-05-06 11:36:38 +0800

    stock chart

--- src/app/line-charts/line-charts.component.ts
+++ src/app/line-charts/line-charts.component.ts
@@ -34,6 +34,7 @@ export class LineChartsComponent implements OnInit {
   ngOnInit(): void {
     this.statisticByCountry = this.serviceRes.statisticByCountry;
     this.loadSevenIndexesChart('China', 'United States'); // default
+    this.loadIndexAndStockChart('China', 'United States'); // default
   }
 
   buildCategoryAndSeriesData(data: any) {
@@ -107,50 +108,89 @@ export class LineChartsComponent implements OnInit {
   }
 
 
-  indexAndStockOption = {
-    color: ['#a71616', '#F15521'],
-    legend: {
-      data: ['Total Confirmed', 'Stock Data'] // TODO switch
-    },
-    tooltip: {
-      trigger: 'axis'
-    },
-    grid: { // position
-      left: '6%',
-      right: '12%',
-      bottom: '5%',
-      containLabel: true
-    },
-    xAxis: [{
-      type: 'category',
-      boundaryGap: false,
-      data: ['01/01/2020','01/15/2020','01/31/2020','02/15/2020','02/29/2020','03/15/2020','03/31/2020','04/15/2020','04/30/2020','05/15/2020'], // TODO api to provide category data
-    }],
-    yAxis: [{
-      type: 'value'
-    }],
-    series: [{
-      name: 'Total Confirmed',
-      smooth: true,
-      type: 'line',
-      label: {
-        normal: {
-          show: true
-        }
-      },
-      data:[8356, 9427, 11060, 35162, 42318, 48134, 79423, 93556, 139236] // TODO api to provide series data
-    }, {
-      name: 'Stock Data',
-      smooth: true,
-      type: 'line',
-      label: {
-        normal: {
-          show: true
-        }
-      },
-      data:[98244, 93462, 71453, 54584, 51087, 41934, 31423, 18832, 13084] // TODO api to provide series data
-    }]
+  buildIndexAndStockDataByCountry(countryName: string) {
+    const indexData = this.buildIndexDataByCountry(countryName);
+    const countryStatistic = this.statisticByCountry.hasOwnProperty(countryName) ? this.statisticByCountry[countryName] : {};
+    const stockData = countryStatistic.stockDataWithDates ? countryStatistic.stockDataWithDates : [];
+    return {
+      "indexData": indexData,
+      "stockData": stockData
+    };
   }
+
+  buildIndexAndStockChartData(indexAndStockData: any) {
+    const chartData_index = this.buildCategoryAndSeriesData(indexAndStockData.indexData);
+    const chartData_stock = this.buildCategoryAndSeriesData(indexAndStockData.stockData);
+    const chartData = {
+      "categories": chartData_index.categoryData,
+      "series": {
+        "indexSeries": chartData_index.seriesData,
+        "stockSeries": chartData_stock.seriesData
+      }
+    };
+    return chartData;
+  }
+
+  buildIndexAndStockChartOption(chartData: any) {
+    return {
+      color: ['#a71616', '#F15521'],
+      legend: {
+        data: [this.indexesMapping[this.selectedIndexId], 'Stock Data']
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      grid: { // position
+        left: '6%',
+        right: '12%',
+        bottom: '5%',
+        containLabel: true
+      },
+      xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+        data: chartData.categories
+      }],
+      yAxis: [{
+        type: 'value'
+      }],
+      series: [{
+        name: this.indexesMapping[this.selectedIndexId],
+        smooth: true,
+        type: 'line',
+        label: {
+          normal: {
+            show: true
+          }
+        },
+        data: chartData.series.indexSeries
+      }, {
+        name: 'Stock Data',
+        smooth: true,
+        type: 'line',
+        label: {
+          normal: {
+            show: true
+          }
+        },
+        data: chartData.series.stockSeries
+      }]
+    };
+  }
+
+  loadIndexAndStockChart(countryName1: string, countryName2: string) {
+    if (countryName1) {
+      const indexAndStockData = this.buildIndexAndStockDataByCountry(countryName1);
+      const chartData = this.buildIndexAndStockChartData(indexAndStockData);
+      this.indexAndStockCountry1 = this.buildIndexAndStockChartOption(chartData);
+    }
+    if (countryName2) {
+      const indexAndStockData = this.buildIndexAndStockDataByCountry(countryName2);
+      const chartData = this.buildIndexAndStockChartData(indexAndStockData);
+      this.indexAndStockCountry2 = this.buildIndexAndStockChartOption(chartData);
+    }
+  }
+
 
   indexAndMacroeOption = {
     color: ['#a71616', '#F6B127'],
@@ -200,6 +240,7 @@ export class LineChartsComponent implements OnInit {
   selectIndex(id: string) {
     this.selectedIndexId = id;
     this.loadSevenIndexesChart(this.selectedCountry1, this.selectedCountry2);
+    this.loadIndexAndStockChart(this.selectedCountry1, this.selectedCountry2);
   }
 
   selectME(id: string) {
@@ -209,10 +250,12 @@ export class LineChartsComponent implements OnInit {
   selectCountry1(name: string) {
     this.selectedCountry1 = name;
     this.loadSevenIndexesChart(name, "");
+    this.loadIndexAndStockChart(name, "");
   }
 
   selectCountry2(name: string) {
     this.selectedCountry2 = name;
     this.loadSevenIndexesChart("", name);
+    this.loadIndexAndStockChart("", name);
   }
 }
